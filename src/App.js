7003b1c709efa41b95e68809aa28b12c2eed6240
Stephen Yao <stephenyao95@gmail.com>
2020-03-09 20:09:08 -0400

    Create barebones for homepage

--- src/App.js
+++ src/App.js
@@ -9,15 +9,26 @@ function App() {
           <img src="./images/textbookify.png" width="200" alt="logo" />
           <div>
             <ul>
-              <li>SEARCH</li>
-              <li>SELL</li>
-              <li>SIGN UP</li>
-              <li>LOG IN</li>
+              <li>Search</li>
+              <li>Sell</li>
+              <li>Log in</li>
+              <li>
+                <button className="button-style">Sign up</button>
+              </li>
             </ul>
           </div>
         </header>
       </section>
-      <section id="main"></section>
+      <section id="main">
+        <div className="main-text">
+          <span>textbookify.</span>
+          <br />
+          Get rid of those old textbooks
+          <br />
+          and get paid.
+        </div>
+      </section>
+      <section></section>
     </div>
   );
 }
