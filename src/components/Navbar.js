a5f57c3596927aab22b58579417296053c8a3d83
Stephen Yao <stephenyao95@gmail.com>
2020-03-17 15:16:05 -0400

    Setup route for login and basic login page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../App.css";
 
 export default class Navbar extends React.Component {
   render() {
     return (
       <header>
-        <img src="./images/textbookify.png" width="250" alt="logo" />
+        <Link to="/">
+          <img src="./images/textbookify.png" width="250" alt="logo" />
+        </Link>
         <div>
           <ul>
             <li>Search</li>
             <li>Sell</li>
-            <li>Log in</li>
+            <Link to="/login">
+              <li>Log in</li>
+            </Link>
             <li>
               <button className="button-style">Sign up</button>
             </li>
--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import Navbar from "../Navbar";
+import "./LoginPage.css";
 
-export default class LoginPage extends React.Component {
-  render() {
-    return <div>Login Page</div>;
+export default function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <div className="Login">
+        <form onSubmit={handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <FormLabel>Email</FormLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <FormLabel>Password</FormLabel>
+            <FormControl
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              type="password"
+            />
+          </FormGroup>
+          <Button block bsSize="large" disabled={!validateForm()} type="submit">
+            Login
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
